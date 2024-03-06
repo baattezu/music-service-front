f2d88cf7a0503f9d8cacb3dbdeb97b75c30a8dd1
baattezu <147482395+baattezu@users.noreply.github.com>
2024-03-06 17:30:22 +0600

    changed

--- src/components/Basic/RequireAuth.js
+++ src/components/Basic/RequireAuth.js
@@ -1,5 +1,5 @@
 import { useLocation, Navigate, Outlet } from "react-router-dom";
-import useAuth from "./Auth/hooks/useAuth";
+import useAuth from "../Auth/hooks/useAuth";
 
 const RequireAuth = ({ allowedRoles }) => {
     const { auth } = useAuth();
